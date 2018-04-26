e5a3542ca0a3b8140880fcf5648e51915c65c0a0
François Parmentier <francois.parmentier@gmail.com>
2018-04-26 11:12:28 +0200

    Add some routes and hapi-pino (logger)

--- index.js
+++ index.js
@@ -7,10 +7,53 @@ const server = Hapi.server({
     host: 'localhost'
 });
 
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => {
+
+      return 'Welcome to ECTOR backend.';
+  }
+});
+
+server.route({
+  method: ['GET', 'POST'],
+  path: '/hello/{name?}',
+  handler: (request, h) => {
+      const user = request.params.name || 'stranger';
+
+      return 'Hello, ' + user + '!';
+  },
+  options: {
+      description: 'Say hello!',
+      notes: 'The user parameter defaults to \'stranger\' if unspecified',
+      tags: ['api', 'greeting']
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/v1/json/{toto}',
+  handler: (request, h) => {
+    return {
+      payload: request.params.toto,
+      length: request.params.toto.length
+    }
+  }
+});
+
 const init = async () => {
 
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
+  await server.register({
+    plugin: require('hapi-pino'),
+    options: {
+      prettyPrint: process.env.NODE_ENV !== 'production',
+      logEvents: ['response']
+    }
+  });
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {

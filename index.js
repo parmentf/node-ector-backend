6ffedfc879174d6fe3fb44e040976df02e68f2a6
François Parmentier <francois.parmentier@gmail.com>
2018-05-05 19:59:56 +0200

    Add post learn route
    And improve error handling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Boom = require('boom');
 const Ector = require('ector');
 const reply = require('./routes/reply');
 const learn = require('./routes/learn');
@@ -8,7 +9,23 @@ const learn = require('./routes/learn');
 const ector = new Ector();
 
 const server = Hapi.server({
-    port: process.env.PORT || 5000
+    port: process.env.PORT || 5000,
+    routes: {
+      validate: {
+        failAction: async (request, h, err) => {
+          if (process.env.NODE_ENV === 'production') {
+            // In prod, log a limited error message and throw the default Bad Request error.
+            console.error('ValidationError:', err.message); // Better to use an actual logger here.
+            throw Boom.badRequest(`Invalid request payload input`);
+          } else {
+            // During development, log and respond with the full error.
+            console.error(err);
+            request.logger.error(err);
+            throw err;
+          }
+        }
+      }
+    }
 });
 
 server.route({
@@ -59,6 +76,23 @@ server.route({
     const { source, entry } = request.params;
     const nodes = learn(ector, source, entry);
     request.logger.info(`/v1/learn/${source}/${entry}`);
+    return h.response(nodes).code(201);
+  },
+  options: {
+    cors: true,
+    description: `Add knowledge to ECTOR's concept network.`,
+    notes: `No activation value added. The source is an identifier of where the knowledge comes from (eg Wikipedia).`,
+    tags: ['api', 'learn']
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/v1/learn/',
+  handler: (request, h) => {
+    const { source, entry } = request.payload;
+    const nodes = learn(ector, source, entry);
+    request.logger.info(`POST /v1/learn/${source}/${entry}`);
     return h.response(nodes).code(201);
   },
   options: {

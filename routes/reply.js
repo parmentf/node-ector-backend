348e6513413b076042361491350e567006106a2b
François Parmentier <francois.parmentier@gmail.com>
2018-09-30 12:20:27 +0200

    :arrow_up: :shirt: Update eslint

--- routes/reply.js
+++ routes/reply.js
@@ -1,10 +1,9 @@
 const previousResponseNodes = {};
 
 module.exports = (ector, user, entry) => {
-  ector.setUser(user);
-  const nodes = ector.addEntry(entry);
-  ector.linkNodesToLastSentence(previousResponseNodes[user]);
-  const response = ector.generateResponse();
-  previousResponseNodes[user] = response.nodes;
-  return response;
+    ector.setUser(user);
+    ector.linkNodesToLastSentence(previousResponseNodes[user]);
+    const response = ector.generateResponse();
+    previousResponseNodes[user] = response.nodes;
+    return response;
 };

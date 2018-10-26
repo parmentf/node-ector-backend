5e7f358dc1a9236e42b4c553f1bc848cd6471c56
François Parmentier <francois.parmentier@gmail.com>
2018-10-26 18:35:59 +0200

    :rotating_light: Prepare tests

---
+++ routes/learn.test.js
@@ -0,0 +1,14 @@
+const server = require('../index');
+
+const defaultRequest = {
+    method: 'POST',
+    url: '/v1/learn',
+    payload: {}
+};
+
+describe('POST /learn', () => {
+    it('should answer with a 200 code', (done) => {
+        const request = { ...defaultRequest }
+        
+    });
+});

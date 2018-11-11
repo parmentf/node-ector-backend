531de4c0cf27d70c7cb55358508398bd8e25e351
François Parmentier <francois.parmentier@gmail.com>
2018-11-11 17:28:29 +0100

    :white_check_mark: Test 404

--- routes/learn.test.js
+++ routes/learn.test.js
@@ -7,11 +7,30 @@ const defaultRequest = {
 };
 
 describe('POST /learn', () => {
-    it('should answer with a 200 code', (done) => {
+    it('should answer with 404 when no payload', (done) => {
         const request = {
             ...defaultRequest,
             payload: {  }
         };
-        expect(server.inject(request)).resolves.toBe();
+        server.inject(request)
+            .then(response => {
+                expect(response.statusCode).toBe(404);
+                done();
+            });
+    });
+
+    it('should answer with a 201 code', (done) => {
+        const request = {
+            ...defaultRequest,
+            payload: {
+                source: 'Source',
+                entry: 'Entry'
+            }
+        };
+        server.inject(request)
+            .then(response => {
+                expect(response.statusCode).toBe(201);
+                done();
+            });
     });
 });

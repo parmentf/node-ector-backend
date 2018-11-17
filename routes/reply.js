04dc090bd436c7b43ed8fdf061a729cfd97b8983
François Parmentier <francois.parmentier@gmail.com>
2018-11-17 22:58:47 +0100

    :bug: Make reply work
    And add tests.

--- routes/reply.js
+++ routes/reply.js
@@ -3,6 +3,7 @@ const previousResponseNodes = {};
 module.exports = (ector, user, entry) => {
     ector.setUser(user);
     ector.linkNodesToLastSentence(previousResponseNodes[user]);
+    ector.addEntry(entry);
     const response = ector.generateResponse();
     previousResponseNodes[user] = response.nodes;
     return response;
